5617ecc9c791574edb7d6935dae4d78a50ba29a7
Eduarda Brisch Dalmás <du.dalmas@hotmail.com>
2024-07-16 21:36:13 -0300

    componente global

--- src/pages/create-trip/steps/destination-and-date-step.tsx
+++ src/pages/create-trip/steps/destination-and-date-step.tsx
@@ -1,4 +1,5 @@
 import { ArrowRight, Calendar, MapPin, Settings2 } from "lucide-react";
+import { Button } from "../../../components/button";
 
 interface DestinationAnDateStepProps {
     isGuestsInputOpen: boolean;
@@ -27,16 +28,17 @@ export function DestinationAnDateStep({
         <div className='w-px h-6 bg-zinc-800' />
 
         {isGuestsInputOpen ? (
-          <button onClick={closeGuestsInput} className='bg-zinc-800 text-zinc-200 rounded px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700'> 
-            Alterar local/data
-            <Settings2 className="size-5" />
-          </button>
+          <Button variant='secondary' onClick={closeGuestsInput}> 
+              Alterar local/data
+              <Settings2 className="size-5" />
+          </Button>
 
         ) : (
-          <button onClick={openGuestsInput} className='bg-lime-300 text-lime-950 rounded px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400'>
-            Continuar
-            <ArrowRight className="size-5" />
-          </button>
+           <Button variant='primary' onClick={openGuestsInput}> 
+              Continuar
+              <ArrowRight className="size-5" />
+          </Button>
+          
         )}
       </div>
     )
